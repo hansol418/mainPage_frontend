5dfcb0ad85bf4a74cdf21f0d0c301d03ca80b7ec
LeeHanSol <hslee4954@gmail.com>
2024-10-31 12:42:39 +0900

    first commit

---
+++ src/main/java/com/busanit501/mainpage_refactoring/MainPageRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.mainpage_refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPageRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainPageRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit501/mainpage_refactoring/MainPageRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.mainpage_refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainPageRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
